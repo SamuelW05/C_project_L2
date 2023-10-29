8781b1a4e7e5be6bd82b2fe2437187b49f4e8f53
AlikBook <117640028+AlikBook@users.noreply.github.com>
2023-10-29 22:05:27 +0100

    Create level_cells.h

---
+++ level_cells.h
@@ -0,0 +1,17 @@
+#ifndef PROJECT1_LEVEL_CELLS_H
+#define PROJECT1_LEVEL_CELLS_H
+
+typedef struct s_d_cell
+{
+    int value;
+    int nb_levels;
+    struct s_d_cell **level;
+} t_d_cell;
+
+typedef t_d_cell* p_cell;
+
+p_cell create_cell(int, int);
+
+#endif
+
+
