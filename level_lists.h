f412ab0b50698f4fe58f271fc6d4f2182f968ea4
AlikBook <117640028+AlikBook@users.noreply.github.com>
2023-10-29 22:06:35 +0100

    Create level_lists.h

---
+++ level_lists.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT1_LEVEL_LISTS_H
+#define PROJECT1_LEVEL_LISTS_H
+#include "level_cells.h"
+typedef struct s_d_list
+{
+    int nb_max_levels;
+    t_d_cell **head;
+} t_d_list;
+
+t_d_list create_empty_list(int);
+void display_cells_at_levels(t_d_list list, int level);
+void display_all_levels(t_d_list list);
+void insert_a_cell(t_d_list* list, int val, int cell_levels);
+void insert_a_cell_with_increasing_order(t_d_list* list, int val, int level, int cell_levels);
+#endif //PROJECT1_LEVEL_LISTS_H
