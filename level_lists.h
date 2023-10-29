63ededcb88ca218ce54b72bbad606f66f1f1ac87
AlikBook <117640028+AlikBook@users.noreply.github.com>
2023-10-29 22:14:41 +0100

    Update level_lists.h

--- level_lists.h
+++ level_lists.h
@@ -11,5 +11,5 @@ t_d_list create_empty_list(int);
 void display_cells_at_levels(t_d_list list, int level);
 void display_all_levels(t_d_list list);
 void insert_a_cell(t_d_list* list, int val, int cell_levels);
-void insert_a_cell_with_increasing_order(t_d_list* list, int val, int level, int cell_levels);
+void insert_a_cell_with_increasing_order(t_d_list* list, int val, int cell_levels);
 #endif //PROJECT1_LEVEL_LISTS_H
